bf54912635fbf4e580f1808fffd0931e0fc0d02c
Lucas Frazao <S123533@cfisd.loc>
2022-10-26 16:13:47 -0500

    This changes everything

--- src/LinkedIntList.java
+++ src/LinkedIntList.java
@@ -1,9 +1,137 @@
 
-public class LinkedIntList {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class LinkedIntList 
+{
+	ListNode front;
+	
+	public LinkedIntList()
+	{
+		front = null;
 	}
 
+	public LinkedIntList(int value) 
+	{
+		front = new ListNode(value);
+	}
+	
+	public void add(int newValue)
+	{
+		ListNode temp = front;
+		if( front == null)
+		{
+			temp = new ListNode(newValue);
+		}
+		else
+		{
+			
+			while( temp.next != null)
+			{
+				temp = temp.next;
+			}
+			
+			temp.next = new ListNode(newValue);
+		}
+	}
+	
+	public void add(int index, int value)
+	{
+		int x = 0;
+		ListNode holder = front;
+		ListNode temp = front;
+		while ( x != index)
+		{
+			temp = temp.next;
+			holder = holder.next;
+			if( x == index)
+			{
+				holder.next = new ListNode(value);
+				holder.next = temp;
+			}
+		}
+		
+		front = holder;
+		
+	}
+	
+	public void get(int index)
+	{
+		int x = 0;
+		ListNode counter = front;
+		
+		while (x != index)
+		{
+			//counter.next;
+		}
+		
+		System.out.println(counter.data);
+	}
+	
+	public void indexOf(int value)
+	{	
+		int index = 0;
+		while (front.next != null)
+		{
+			front = front.next;
+			if (front.data == value)
+			{
+				System.out.println(index);
+			}
+			else
+			{
+				index++;
+			}
+		}
+		
+	}
+	
+	public int remove(int index)
+	{
+		ListNode temp1 = new ListNode();
+		ListNode temp2 = new ListNode();
+		ListNode temp3 = new ListNode();
+		temp1 = front;
+		temp2 = front;
+		
+		
+		for(int x = 0; x < index; x++)
+		{
+			temp1 = temp1.next;
+			temp2 = temp1.next.next;
+			temp3 = temp3.next;
+			
+		}
+		
+		temp1.next = temp2;
+		front = temp1;
+		return temp3.data;
+	}
+	
+	public int size()
+	{
+		int count = 0;
+		ListNode temp = new ListNode();
+		
+		while(temp.next != null)
+		{
+			temp = temp.next;
+			count ++;
+		}
+		
+		return count;
+	}
+	
+	public String toString()
+	{
+		String x = "";
+		ListNode temp = new ListNode();
+		while(temp != null)
+		{
+			x = x + temp.data;
+			temp = temp.next;
+		}
+		return x;
+		
+	}
+	
+	
+	
 }
--- src/LinkedListReplicated.java
+++ src/LinkedListReplicated.java
@@ -3,22 +3,29 @@
  * 
  */
 
-import java.io.File;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.IOException;
 
 
-public class LinkedListReplicated  {
+public class LinkedListReplicated  
+{
  
     public static void main(String s[]) 
     {
     	
 		//Students create 2 LinkedIntList objects.
-    		// a) Uses the default constructor
-    		// b) Uses the the construtor that allows an integer value to be passed in.
+    	
+            LinkedIntList two = new LinkedIntList();
+    		LinkedIntList one = new LinkedIntList(1);
     	
 		
       	//Test out each of your various methods (add, insert, get, indexOf, remove, size, toString)) 
-      		//using either of the instantiated LinkedIntList objects
+      	//using either of the instantiated LinkedIntList objects
+    		
+    		one.add(1);
+    		System.out.println(one.toString());
+    		two.add(1);
+    		System.out.println(two);
     }
     
 	//Create your LinkedIntList class in another file called "LinkedIntList"
